94198c7cd4207d335c9865d8904f62cf17856ab7
= <=>
2023-07-29 22:10:41 -0700

    Organizing Code with Import/Export Lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,16 @@
+
+
 import React from "react";
+import { image } from "../data/user";
 
 function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>I Made This</p>
-      <img src={image} alt="I made this" />
+      <p>I am passionate about web development and always eager to learn more.</p>
+      <img src={image} alt="A representation of me" />
     </div>
   );
 }
+
+export default About;
